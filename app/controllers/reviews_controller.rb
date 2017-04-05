b51f34f74a9a8506e7f0c2251bdfb3a88b2c7aa1
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-05 12:09:08 -0400

    fleshed out routes and controllers for restaurant and reivew

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,19 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def review_params
+    params.require(review).permit(:content, :rating)
+  end
+end
