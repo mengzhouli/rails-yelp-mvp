086c0af9317d268ebe9ae792322287cd3e6aef87
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-05 14:44:14 -0400

    everything works

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,19 +1,20 @@
 class ReviewsController < ApplicationController
 
-  def new
-    @review = Review.new
-  end
-
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     @review.save
-    redirect_to restaurant_path(@restaurant)
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
 
   def review_params
-    params.require(review).permit(:content, :rating)
+    params.require(:review).permit(:content, :rating)
   end
 end
