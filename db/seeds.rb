747ed6ffb616be25e36b1f896032865dc539f8da
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-05 11:36:53 -0400

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  resto = Restaurant.create(
+    name: ["pfk", "mcD", "AuCoq", "Aux Vivres", "Gus", "Belle Province", "Chez Boris"].sample,
+    address: ["john st", "river st", "ave blvd", "rue andre", "rue cath", "chemin mary"].sample,
+    phone_number: rand(4000000..9999999),
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+
+  5.times do
+    rev = Review.new(
+      content: ["This is a shite resto, don't come here", "The rat king lives here", "Terrible Service", "You get what you paid for", "My fav", "Eat here if you want to die"].sample,
+      rating: rand(0..5)
+    )
+    rev.restaurant = resto
+    rev.save!
+  end
+end
