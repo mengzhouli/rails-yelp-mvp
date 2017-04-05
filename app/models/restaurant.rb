6472c241bbf39e604142e84cdba0dd6d742be147
Mengzhou Li <mengzhou.li@gmail.com>
2017-04-05 14:03:35 -0400

    added views for restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,5 +2,5 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false}
+  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false}
 end
